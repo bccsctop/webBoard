3f941e7a82cdaad279f7c85872276cefc4bdfedc
Nut-Wtp <s6101012630071@email.kmutnb.ac.th>
2021-04-01 15:13:22 +0700

    add Create Function

--- webBoard/core/views.py
+++ webBoard/core/views.py
@@ -3,14 +3,15 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
 from webBoard.core.forms import create_topic_form , create_comment_form
-from webBoard.core.models import Topic
+from webBoard.core.models import Topic , Comment
 
 # Create your views here.
 def home(request):
     count = User.objects.count()
     topic_all = Topic.objects.all()
+    comment_all = Comment.objects.all()
     return render(request, 'home.html', {
-        'count': count,'topic_all': topic_all
+        'count': count,'topic_all': topic_all , 'comment_all' : comment_all,
     })
 
 def signup(request):
@@ -46,20 +47,22 @@ def create_topic(request):
     })
 
 def create_comment(request):
-    topic_id = request.GET.get('topicid')
+    topic_idoo = request.GET.get('topicid')
     if not request.user.is_authenticated:
         return HttpResponseRedirect("accounts/login")
     elif request.method == 'POST':
+        idtop = request.POST.get('fkid')
+        tpid = Topic.objects.get(topicid= idtop)
         form = create_comment_form(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.comuserid = request.user
+            comment.comtopicid = tpid
             comment.like = 0
-            comment.comtopicid = topic_id
             comment.save()
         return HttpResponseRedirect("/")
     else:
         form = create_comment_form()
         return render(request, 'create_comment.html', {
-        'form': form , 'topic_id' : topic_id ,
+        'form': form , 'topic_id' : topic_idoo ,
     })
